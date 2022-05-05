7f81bb0734c5a971eeb4523b50bb18e170902ca1
Joseph Heetel <joseph.heetel@protonmail.com>
2022-05-05 16:02:42 +0200

    Set CWD via CMAKE configure_file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "sponza_sample.hpp"
+#include "configurepath.cmakegenerated.hpp"
+#include <hsk_env.hpp>
 
 int main(int argv, char** args)
 {
+    hsk::OverrideCurrentWorkingDirectory(CWD_OVERRIDE_PATH);
     ImportanceSamplingRtProject project;
     return project.Run();
 }
